d95d40cad38a7735799108e861e27407927924ff
bmeloski <bartlomiej.skoraczewski@gmail.com>
2022-07-09 14:23:17 +0200

    feat: setup routing

--- src/Presentation/Component/Company/Controller/CompanyList/CompanyListController.php
+++ src/Presentation/Component/Company/Controller/CompanyList/CompanyListController.php
@@ -6,11 +6,9 @@ namespace App\Presentation\Component\Company\Controller\CompanyList;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyListController extends AbstractController
 {
-    #[Route(path: '/company', name: 'company_list', methods: ['GET'])]
     public function list(): Response
     {
         return new Response('Welcome to Latte and Code ');
--- src/Presentation/Component/Company/Controller/CompanyShow/CompanyShowController.php
+++ src/Presentation/Component/Company/Controller/CompanyShow/CompanyShowController.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace App\Presentation\Component\Company\Controller\CompanyShow;
 
+use App\Core\Component\Company\Domain\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyShowController extends AbstractController
 {
-    #[Route(path: '/company/{company}', name: 'company_show', methods: ['GET'])]
-    public function list($company): Response
+    public function show(Company $company): Response
     {
-        echo $company;
-
         return new Response('Welcome to Latte and Code ');
     }
 }
